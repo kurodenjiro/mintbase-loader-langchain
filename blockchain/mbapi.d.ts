86c658824feeff722d7545a48100e71b886586ec
Kuro Denjiro <kurodenjiro1@gmail.com>
2024-03-05 17:11:28 +0700

    Update mbapi.d.ts

--- blockchain/mbapi.d.ts
+++ blockchain/mbapi.d.ts
@@ -1,6 +1,6 @@
 import { Document } from "@langchain/core/documents";
 import { BaseDocumentLoader } from "langchain/document_loaders/base.js";
-import { SearchApiLoader } from "langchain/document_loaders/web/searchapi";
+
 type JSONPrimitive = string | number | boolean | null;
 type JSONValue = JSONPrimitive | JSONObject | JSONArray;
 interface JSONObject {
@@ -9,13 +9,9 @@ interface JSONObject {
 interface JSONArray extends Array<JSONValue> {
 }
 /**
- * SearchApiParameters Type Definition.
+ * MintbaseParameters Type Definition.
  *
  * For more parameters and supported search engines, refer specific engine documentation:
- * Google - https://www.searchapi.io/docs/google
- * Google News - https://www.searchapi.io/docs/google-news
- * Google Scholar - https://www.searchapi.io/docs/google-scholar
- * YouTube Transcripts - https://www.searchapi.io/docs/youtube-transcripts
  * and others.
  *
  */
@@ -24,13 +20,17 @@ type apiKey = string;
 type blockchainType = string;
 /**
  * Class representing a document loader for loading search results from
- * the SearchApi. It extends the BaseDocumentLoader class.
+ * the Mintbase. It extends the BaseDocumentLoader class.
  * @example
  * ```typescript
+ * const BlockchainType = {
+ *  NEAR_MAINNET: "mainnet",
+ *  NEAR_TESTNET: "testnet",
+ *};
  * const loader = new MintBaseLoader({
- *   q: "{query}",
+ *   contractAddress: "{contractAddress}",
  *   apiKey: "{apiKey}",
- *   engine: "google",
+ *   blockchainType: "{blockchainType.NEAR_MAINNET}",
  * });
  * const docs = await loader.load();
  * ```
@@ -39,24 +39,14 @@ export declare class MintBaseLoader extends BaseDocumentLoader {
     private apiKey;
     private contractAddress;
     constructor(contractAddress: contractAddress,apiKey:apiKey,blockchainType:blockchainType);
-    /**
-     * Builds the URL for the SearchApi search request.
-     * @returns The URL for the search request.
-     */
-    buildUrl(): string;
+
     /**
      * Extracts documents from the provided output.
      * @param output - The output to extract documents from.
-     * @param responseType - The type of the response to extract documents from.
      * @returns An array of Documents.
      */
     private extractDocuments;
-    /**
-     * Processes the response data from the SearchApi search request and converts it into an array of Documents.
-     * @param data - The response data from the SearchApi search request.
-     * @returns An array of Documents.
-     */
-    processResponseData(data: Record<string, unknown>): Document[];
+
     /**
      * Fetches the data from the provided URL and returns it as a JSON object.
      * If an error occurs during the fetch operation, an exception is thrown with the error message.
@@ -66,7 +56,7 @@ export declare class MintBaseLoader extends BaseDocumentLoader {
      */
     private fetchData;
     /**
-     * Loads the search results from the SearchApi.
+     * Loads the search results from the Mintbase.
      * @returns An array of Documents representing the search results.
      * @throws An error if the search results could not be loaded.
      */
