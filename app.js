35ae977c99938e9482d9f064ef49936fc2024160
Kuro Denjiro <kurodenjiro1@gmail.com>
2024-03-06 05:18:24 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,51 @@
-import { MintBaseLoader } from './blockchain/mbapi.js'
 
-const blockchainType = {
-    NEAR_MAINNET: "mainnet",
-    NEAR_TESTNET: "testnet",
-};
-export const run = async () => {
-    const loader = new MintBaseLoader("mint.yearofchef.near", "omni-site", blockchainType.NEAR_MAINNET);
-    const docs = await loader.load();
-    console.log(docs);
-};
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { HNSWLib } from "langchain/vectorstores/hnswlib";
+import { createRetrieverTool } from "langchain/tools/retriever";
+import { AgentExecutor, createOpenAIToolsAgent } from "langchain/agents";
+import { ChatOpenAI } from "@langchain/openai";
+import {
+    ChatPromptTemplate,
+    MessagesPlaceholder,
+} from "@langchain/core/prompts";
+import {} from 'dotenv/config'
 
-run();
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
+const llm = new ChatOpenAI({ openAIApiKey: OPENAI_API_KEY, temperature: 0 });
+async function getAnswer(question) {
+
+    // STEP 1: Load the vector store
+    const vectorStore = await HNSWLib.load(
+        "hnswlib",
+        new OpenAIEmbeddings({ openAIApiKey: OPENAI_API_KEY }),
+    );
+
+    const retriever = vectorStore.asRetriever();
+    const tool = await createRetrieverTool(retriever, {
+        name: "near_nft_mainnet",
+        description: "Searches contract address and returns NFT general information.",
+    });
+
+    const tools = [tool];
+    const prompt = ChatPromptTemplate.fromMessages([
+        ["system", "You are very powerful assistant, but don't know current events"],
+        ["human", "{input}"],
+        new MessagesPlaceholder("agent_scratchpad"),
+    ]);
+
+    const agent = await createOpenAIToolsAgent({
+        llm,
+        tools,
+        prompt,
+    });
+    const agentExecutor = new AgentExecutor({
+        agent,
+        tools
+    });
+    const result = await agentExecutor.invoke({
+        input: question
+    });
+    console.log(result)
+}
+getAnswer("mint.yearofchef.near")
